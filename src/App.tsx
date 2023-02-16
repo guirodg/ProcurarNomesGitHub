ab27ece9c60c9569bc6050e4a36778979a9fd750
guirodg <guilherme.rddev@gmail.com>
2023-02-16 16:43:57 -0300

    adiciona estilos

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import {Outlet} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Github Finder</h1>
+    <div className="App max-w-[500px] mx-auto my-0">
+      <h1 className="text-center mb-4">Github Finder</h1>
       <Outlet/>
     </div>
   );
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -8,14 +8,24 @@ type SearchProps = {
 const Search = ({loadUser}: SearchProps) => {
   const [userName, setUserName] = useState("");
   
+  const handleKeyDown = (e: any) => {
+    if (e.key === "Enter") {
+      return loadUser(userName);
+    }
+  }
+  
   return (
-    <div>
+    <div className="bg-[#2b3566] p-8 rounded-2xl flex flex-col justify-center items-center gap-4 mb-6">
       <h2>Busque por um usuário:</h2>
-      <p>Conheça seus melhores repositórios</p>
-      <div>
-        <input type="text" placeholder='Digite o nome do usuário' onChange={(e) => setUserName(e.target.value)}
+      <p className="text-[#9da5d1]">Conheça seus melhores repositórios</p>
+      <div className="flex gap-2">
+        <input
+          className="p-2.5 rounded-[3px] border-[none] text-[#2b3566]"
+          type="text" placeholder='Digite o nome do usuário'
+          onChange={(e) => setUserName(e.target.value)}
+          onKeyDown={handleKeyDown}
         />
-        <button onClick={() => loadUser(userName)}>
+        <button className="p-3.5 bg-[#0e1129] rounded" onClick={() => loadUser(userName)}>
           <BsSearch/>
         </button>
       </div>
