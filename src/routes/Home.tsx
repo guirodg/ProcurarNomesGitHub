a1216553b633a6f8a71135ac4421ec77378659a5
guirodg <guilherme.rddev@gmail.com>
2023-02-16 17:24:34 -0300

    adiciona logica para mostrar componente erro

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,13 +2,23 @@ import React, {useState} from 'react'
 import Search from "../components/Search"
 import {UserProps} from "../types/user"
 import User from "../components/User";
+import Error from "../components/Error";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
   
   const loadUser = async (userName: string) => {
+    setError(false)
+    setUser(null)
+    
     const res = await fetch(`https://api.github.com/users/${userName}`)
-    let data = await res.json()
+    const data = await res.json()
+    
+    if (res.status === 404) {
+      setError(true)
+      return
+    }
     
     const {avatar_url, login, location, followers, following} = data
     const userData: UserProps = {
@@ -27,6 +37,7 @@ const Home = () => {
     <div>
       <Search loadUser={loadUser}/>
       {user && <User{...user}/>}
+      {error && <Error/>}
     </div>
   )
 }
